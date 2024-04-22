c2d38c7d71c3eb0da6352497ab293551e7ca070a
Sushovit204 <shahsushovit434@gmail.com>
2024-04-22 20:22:38 +0545

    Updated and documented the code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# web_scraping_application
-The objective of project is to create a Python application that performs web scraping to extract data from a website and stores it in a structured format.
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Prerequisites](#prerequisites)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+This Python application scrapes book data from a specified website and stores it in a SQLite database. It is designed to extract book titles, prices, and ratings from a books website URL and save this information in a structured format for further analysis or use.
+
+
+## Prerequisites
+
+
+## Installation
+Before installing the project your system must have python and pip installed.
+
+To install the project, run the following command:
+1. Make virtual environment using `py3 -m venv venv`
+2. Activate virtual environment using `Source venv/Scripts/activate`
+3. Install the project dependencies using `pip install -r requirements.txt`
--- main.py
+++ main.py
@@ -6,47 +6,71 @@ import logging
 # Configure logging
 logging.basicConfig(filename='scraping_errors.log', level=logging.ERROR)
 
-def scrape_and_store_movies(url, db_file):
+def scrape_and_store_books(url, db_file):
+    """
+    Scrape Book title,price and rating from the given books website sandbox URL and store it in a SQLite database.
+
+    Parameters:
+    - url (str): The URL of the Book page containing books and prices.
+    - db_file (str): The filename of the SQLite database to store the data.
+
+    Returns:
+    None
+    """
     try:
         # Connect to SQLite database (create if not exists)
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
 
-        # Create table to store movie data
-        cursor.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, title TEXT, year TEXT, rating REAL)")
+        # Create table to store product data
+        cursor.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, title TEXT, price TEXT, rating TEXT)")
+
 
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for non-200 status codes
-        soup = BeautifulSoup(response.text, 'html.parser')
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        movies = soup.select('td.titleColumn')
-        ratings = soup.select('td.ratingColumn.imdbRating')
+        # Find all book containers
+        book_containers = soup.find_all("article", class_="product_pod")
 
-        # Iterate over movies and insert data into table
-        for movie, rating in zip(movies, ratings):
-            title = movie.select('a')[0].text
-            year = movie.select('span')[0].text[1:-1]
-            rating = float(rating.select('strong')[0].text)
+        # Loop through each book container and extract title and price
+        for book in book_containers:
+            # Extract the title
+            title = book.h3.a.get_text(strip=True)
 
-            # Insert movie data into table
-            sql = "INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)"
-            val = (title, year, rating)
+            # Extract the price
+            price = book.find("p", class_="price_color").get_text(strip=True)
+
+            # Extract the rating
+            rating_class = book.find("p", class_="star-rating")
+            rating = rating_class['class'][1]
+
+            # Insert product data into table
+            sql = "INSERT INTO products (title, price, rating) VALUES (?, ?, ?)"
+            val = (title, price, rating)
             cursor.execute(sql, val)
             conn.commit()
 
-        print("Movies added to SQLite database!")
+        print("Data has been committed to the database")
 
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching data from {url}: {e}")
+        print("log error")
+    except sqlite3.Error as e:
+        logging.error(f"SQLite error: {e}")
+        print("SQLite error:", e)
     except Exception as e:
         logging.error(f"Error processing data: {e}")
+        print(f"second log error {e}")
     finally:
         # Close the connection
         if 'conn' in locals():
             conn.close()
 
-# Test the function with IMDb's top-rated movies URL and SQLite database file
+# Test the function with Amazon's top sale products URL and SQLite database file
 if __name__ == "__main__":
-    imdb_url = 'https://www.imdb.com/chart/top/?sort=ir,desc&mode=simple&page=1'
-    sqlite_db_file = 'movies.db'
-    scrape_and_store_movies(imdb_url, sqlite_db_file)
+    book_site_url = 'http://books.toscrape.com/'
+    sqlite_db_file = 'books.db'
+
+    scrape_and_store_books(book_site_url, sqlite_db_file)
